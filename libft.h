0511221f5c203a866bf6ef28eb341cc4884e0946
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-06-01 07:20:42 +0200

    added strchar_idx

--- libft.h
+++ libft.h
@@ -82,6 +82,7 @@ char				*ft_strpull_ptr(const char **s, char c);
 int					ft_strcount(char const *s, char d);
 int					ft_wordlen(char const *s, char d);
 int					ft_intlen(int n);
+int					ft_strchr_idx(const char *str, int c);
 
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
